81b9baf6276b45c9ca5ab0483e09f82ce252f58e
Thomas Stahl <Thomas.Stahl@covance.com>
2020-03-31 12:20:15 -0400

    Add project files.

---
+++ RegistrationFormCore/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RegistrationFormCore.Models;
+
+namespace RegistrationFormCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GetDetails()
+        {
+            UserDataModel umodel = new UserDataModel();
+            umodel.Name = HttpContext.Request.Form["txtName"].ToString();
+            umodel.Age = Convert.ToInt32(HttpContext.Request.Form["txtAge"]);
+            umodel.City = HttpContext.Request.Form["txtCity"].ToString();
+            int result = umodel.SaveDetails();
+            if (result > 0)
+            {
+                ViewBag.Result = "Data Saved Successfully";
+            }
+            else
+            {
+                ViewBag.Result = "Something Went Wrong";
+            }
+            return View("Profile");
+        }
+    }
+}
---
+++ RegistrationFormCore/Models/GetConString.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+ 
+namespace RegistrationFormCore.Models
+{
+    public static class GetConString
+    {
+        public static string ConString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var config = builder.Build();
+            string constring = config.GetConnectionString("UserProfile");
+            return constring;
+        }
+    }
+}
